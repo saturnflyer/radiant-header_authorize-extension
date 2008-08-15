eee68a3bb16ab75ab4e248727391dce68a4d1343
Jim Gay <jim@saturnflyer.com>
2008-08-15 15:46:52 -0400

    corrected header call

--- lib/header_authorization.rb
+++ lib/header_authorization.rb
@@ -11,24 +11,24 @@ module HeaderAuthorization
   
   def authenticate_with_header
     unless self.current_user
-      if !headers[auth_header].blank?
-        self.current_user = User.send("find_by_#{auth_attr}",headers[auth_header])
+      if !request.env[auth_header].blank?
+        self.current_user = User.send("find_by_#{auth_attr}",request.env[auth_header])
       end
     end
   end
   
   def auth_header
-    Radiant::Config[HEADER_AUTHORIZE_KEY].downcase.to_sym || :user_id
+    Radiant::Config[HEADER_AUTHORIZE_KEY] || 'HTTP_USER_ID'
   end
   
   def auth_attr
-    Radiant::Config[HEADER_AUTHORIZE_ATTR].downcase || 'login'
+    Radiant::Config[HEADER_AUTHORIZE_ATTR] || 'login'
   end
   
   # Stub out header
   def stubbed_header_for_development
     if RAILS_ENV == 'development' && !Radiant::Config[HEADER_AUTHORIZE_DEV_ACCOUNT].blank?
-      headers[auth_header] = Radiant::Config[HEADER_AUTHORIZE_DEV_ACCOUNT]
+      request.env[auth_header] = Radiant::Config[HEADER_AUTHORIZE_DEV_ACCOUNT]
     end
   end
 end
