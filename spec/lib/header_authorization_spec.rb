760b839910a8a4b15d8808c6f100dc58fbdecbc7
Jim Gay <jim@saturnflyer.com>
2008-08-16 23:56:07 -0400

    adding some specs

--- spec/lib/header_authorization_spec.rb
+++ spec/lib/header_authorization_spec.rb
@@ -1,1 +1,26 @@
 require File.dirname(__FILE__) + '/../spec_helper'
+
+describe ApplicationController, "with header authorization" do
+  scenario :users
+  
+  before(:each) do
+    Radiant::Config['authorization.header'] == 'HTTP_USER_ID'
+    Radiant::Config['authorization.attribute'] == 'login'
+    @controller = ApplicationController.new
+    @request = ActionController::TestRequest.new
+    @response = ActionController::TestResponse.new
+    @request.env['HTTP_USER_ID'] == 'Admin'
+    get '/admin'
+  end
+  
+  it "should get the header for authorization from Radiant::Config['authorization.header']" do
+    @controller.auth_header.should == 'HTTP_USER_ID'
+  end
+  it "should get the user attribute for authorization from Radiant::Config['authorization.attribute']" do
+    @controller.auth_attr.should == 'login'
+  end
+  it "should set the current user by comparing authorization.header and authorization.attribute from the users"
+  describe "with Radiant::Config['authorization.users.create'] set to true" do
+    it "should create a new user if the user with the authorization.header is not found"
+  end
+end
