1a0b34654fde1f2685a0bf5e9c15261b9a856600
Jim Gay <jim@saturnflyer.com>
2008-08-16 23:54:39 -0400

    fixed typo, added user_init constant

--- header_authorize_extension.rb
+++ header_authorize_extension.rb
@@ -2,6 +2,7 @@ require_dependency 'application'
 HEADER_AUTHORIZE_KEY = 'authorization.header'
 HEADER_AUTHORIZE_ATTR = 'authorization.attribute'
 HEADER_AUTHORIZE_DEV_ACCOUNT = 'authorization.development.account'
+HEADER_AUTHORIZE_USER_INIT = 'authorization.users.create'
 
 class HeaderAuthorizeExtension < Radiant::Extension
   version "1.0"
@@ -13,7 +14,7 @@ class HeaderAuthorizeExtension < Radiant::Extension
       include HeaderAuthorization
     }
     if Radiant::Config[HEADER_AUTHORIZE_KEY].blank?
-      Radiant::Config[HEADER_AUTHORIZE_KEY] = 'USER_ID'
+      Radiant::Config[HEADER_AUTHORIZE_KEY] = 'HTTP_USER_ID'
       auth_config = Radiant::Config.find_by_key(HEADER_AUTHORIZE_KEY)
       if auth_config.respond_to?(:description)
         auth_config.update_attribute :description, "This header will be used to authenticate users against this application's database. You'll need to restart for changes to take effect."
@@ -23,7 +24,7 @@ class HeaderAuthorizeExtension < Radiant::Extension
       Radiant::Config[HEADER_AUTHORIZE_ATTR] = 'login'
       auth_config = Radiant::Config.find_by_key(HEADER_AUTHORIZE_ATTR)
       if auth_config.respond_to?(:description)
-        auth_config.update_attribute :description, "This attrubite on the user record will be used to authenticate against the HTTP header. You'll need to restart for changes to take effect."
+        auth_config.update_attribute :description, "This attribute on the user record will be used to authenticate against the HTTP header. You'll need to restart for changes to take effect."
       end
     end
     if RAILS_ENV == 'development'
