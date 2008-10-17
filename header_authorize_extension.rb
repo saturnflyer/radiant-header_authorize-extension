19912027a482c6a02cf25c72b39cb509ab02b133
Jim Gay <jim@saturnflyer.com>
2008-10-17 00:39:31 -0400

    allow new users to be created

--- header_authorize_extension.rb
+++ header_authorize_extension.rb
@@ -13,30 +13,38 @@ class HeaderAuthorizeExtension < Radiant::Extension
     ApplicationController.class_eval %{
       include HeaderAuthorization
     }
-    if Radiant::Config[HEADER_AUTHORIZE_KEY].blank?
-      Radiant::Config[HEADER_AUTHORIZE_KEY] = 'HTTP_USER_ID'
-      auth_config = Radiant::Config.find_by_key(HEADER_AUTHORIZE_KEY)
-      if auth_config.respond_to?(:description)
-        auth_config.update_attribute :description, "This header will be used to authenticate users against this application's database. You'll need to restart for changes to take effect."
-      end
-    end
-    if Radiant::Config[HEADER_AUTHORIZE_ATTR].blank?
-      Radiant::Config[HEADER_AUTHORIZE_ATTR] = 'login'
-      auth_config = Radiant::Config.find_by_key(HEADER_AUTHORIZE_ATTR)
-      if auth_config.respond_to?(:description)
-        auth_config.update_attribute :description, "This attribute on the user record will be used to authenticate against the HTTP header. You'll need to restart for changes to take effect."
-      end
-    end
-    if RAILS_ENV == 'development'
-      if Radiant::Config[HEADER_AUTHORIZE_DEV_ACCOUNT].blank?
-        Radiant::Config[HEADER_AUTHORIZE_DEV_ACCOUNT] = User.find(:first)[:login]
-        auth_config = Radiant::Config.find_by_key(HEADER_AUTHORIZE_DEV_ACCOUNT)
+    if ActiveRecord::Base.connection.tables.include?('config')
+      if Radiant::Config[HEADER_AUTHORIZE_KEY].blank?
+        Radiant::Config[HEADER_AUTHORIZE_KEY] = 'HTTP_USER_ID'
+        auth_config = Radiant::Config.find_by_key(HEADER_AUTHORIZE_KEY)
         if auth_config.respond_to?(:description)
-          auth_config.update_attribute :description, "This value will be used in development to mock the user authorization against the HTTP header. Set an empty value to disable the mock authorization. You'll need to restart for changes to take effect."
+          auth_config.update_attribute :description, "This header will be used to authenticate users against this application's database. You'll need to restart for changes to take effect."
+        end
+      end
+      if Radiant::Config[HEADER_AUTHORIZE_ATTR].blank?
+        Radiant::Config[HEADER_AUTHORIZE_ATTR] = 'login'
+        auth_config = Radiant::Config.find_by_key(HEADER_AUTHORIZE_ATTR)
+        if auth_config.respond_to?(:description)
+          auth_config.update_attribute :description, "This attribute on the user record will be used to authenticate against the HTTP header. You'll need to restart for changes to take effect."
+        end
+      end
+      if Radiant::Config[HEADER_AUTHORIZE_USER_INIT].blank?
+        Radiant::Config[HEADER_AUTHORIZE_USER_INIT] = true
+        auth_config = Radiant::Config.find_by_key(HEADER_AUTHORIZE_USER_INIT)
+        if auth_config.respond_to?(:description)
+          auth_config.update_attribute :description, "When this is set to true, any authorized use that enters will have a user account created for them. To disable this, set the value to 'false'"
+        end
+      end
+      if RAILS_ENV == 'development'
+        if Radiant::Config[HEADER_AUTHORIZE_DEV_ACCOUNT].blank?
+          Radiant::Config[HEADER_AUTHORIZE_DEV_ACCOUNT] = User.find(:first)[:login]
+          auth_config = Radiant::Config.find_by_key(HEADER_AUTHORIZE_DEV_ACCOUNT)
+          if auth_config.respond_to?(:description)
+            auth_config.update_attribute :description, "This value will be used in development to mock the user authorization against the HTTP header. Set an empty value to disable the mock authorization. You'll need to restart for changes to take effect."
+          end
         end
       end
     end
-    
   end
   
 end
--- lib/header_authorization.rb
+++ lib/header_authorization.rb
@@ -7,14 +7,6 @@ module HeaderAuthorization
     super
   end
   
-  def authenticate_with_header
-    unless self.current_user
-      if !request.env[auth_header].blank?
-        self.current_user = User.send("find_by_#{auth_attr}",request.env[auth_header])
-      end
-    end
-  end
-  
   def auth_header
     Radiant::Config[HEADER_AUTHORIZE_KEY] || 'HTTP_USER_ID'
   end
@@ -23,9 +15,25 @@ module HeaderAuthorization
     Radiant::Config[HEADER_AUTHORIZE_ATTR] || 'login'
   end
   
+  def authenticate_with_header
+    unless self.current_user
+      if !request.env[auth_header].blank?
+        if self.current_user = User.send("find_by_#{auth_attr}",request.env[auth_header])
+          self.send(:set_current_user)
+        else
+          if Radiant::Config[HEADER_AUTHORIZE_USER_INIT]
+            self.current_user = User.create!(:login => request.env[auth_header], :name => request.env[auth_header], :password => request.env[auth_header], :password_confirmation => request.env[auth_header])
+            self.send(:set_current_user)
+            flash[:notice] = 'Welcome. A new account has been created for you.'
+          end
+        end
+      end
+    end
+  end
+  
   # Stub out header
   def stub_header_for_development
-    if RAILS_ENV == 'development' && !Radiant::Config[HEADER_AUTHORIZE_DEV_ACCOUNT].blank?
+    if RAILS_ENV.match(/development|local/) && !Radiant::Config[HEADER_AUTHORIZE_DEV_ACCOUNT].blank?
       request.env[auth_header] = Radiant::Config[HEADER_AUTHORIZE_DEV_ACCOUNT]
     end
   end
